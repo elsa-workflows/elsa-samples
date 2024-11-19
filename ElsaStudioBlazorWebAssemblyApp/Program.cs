6b281a89a42469782a93929c8f1ba189dfde8bc4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-11-19 12:05:36 +0100

    Add ElsaStudioBlazorWebAssemblyApp project
    Introduce a new Blazor WebAssembly project to the solution. This includes setting up necessary using directives, project configuration, launch settings, and initial program setup. Also, update solution file and directory structure to accommodate the new project and renamed existing Blazor Server project for consistency.

---
+++ ElsaStudioBlazorWebAssemblyApp/Program.cs
@@ -0,0 +1,43 @@
+using Elsa.Studio.Contracts;
+using Elsa.Studio.Core.BlazorWasm.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
+using Elsa.Studio.Extensions;
+using Elsa.Studio.Localization.Time;
+using Elsa.Studio.Localization.Time.Providers;
+using Elsa.Studio.Login.BlazorWasm.Extensions;
+using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Models;
+using Elsa.Studio.Shell;
+using Elsa.Studio.Shell.Extensions;
+using Elsa.Studio.WorkflowContexts.Extensions;
+using Elsa.Studio.Workflows.Designer.Extensions;
+using Elsa.Studio.Workflows.Extensions;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var configuration = builder.Configuration;
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.RegisterCustomElsaStudioElements();
+
+var backendApiConfig = new BackendApiConfig
+{
+    ConfigureBackendOptions = options => configuration.GetSection("Backend").Bind(options),
+    ConfigureHttpClientBuilder = options => options.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler), 
+};
+
+builder.Services.AddCore();
+builder.Services.AddShell();
+builder.Services.AddRemoteBackend(backendApiConfig);
+builder.Services.AddLoginModule();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
+builder.Services.AddWorkflowContextsModule();
+builder.Services.AddScoped<ITimeZoneProvider, LocalTimeZoneProvider>();
+
+var app = builder.Build();
+var startupTaskRunner = app.Services.GetRequiredService<IStartupTaskRunner>();
+await startupTaskRunner.RunStartupTasksAsync();
+await app.RunAsync();
