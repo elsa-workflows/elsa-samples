8581a34a0ef357a66568a3600c7246743500e0a4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-05-17 19:48:13 +0200

    Add workflow instance pages and layout improvements
    Introduced new workflow instance listing and viewer pages, using `ElsaStudioLayout`. Added `ContentLayout` for common layout consistency across pages. Enhanced app configuration with localization, backend API integration, and new navigation updates in the main menu.

--- src/blazor/ElsaStudioRehostedBlazorComponents/ElsaStudioRehostedBlazorComponents/Program.cs
+++ src/blazor/ElsaStudioRehostedBlazorComponents/ElsaStudioRehostedBlazorComponents/Program.cs
@@ -1,6 +1,12 @@
+using Elsa.Api.Client.HttpMessageHandlers;
+using Elsa.Api.Client.Options;
 using Elsa.Studio.Contracts;
 using Elsa.Studio.Core.BlazorWasm.Extensions;
 using Elsa.Studio.Extensions;
+using Elsa.Studio.Localization.BlazorWasm.Extensions;
+using Elsa.Studio.Localization.Models;
+using Elsa.Studio.Models;
+using Elsa.Studio.Options;
 using Elsa.Studio.Shell.Extensions;
 using Elsa.Studio.Workflows.Extensions;
 using ElsaStudioRehostedBlazorComponents.Client.Services;
@@ -8,13 +14,33 @@ using ElsaStudioRehostedBlazorComponents.Components;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+builder.Services.Configure<ElsaClientOptions>(configuration.GetSection("WorkflowServer"));
+builder.Services.Configure<BackendOptions>(configuration.GetSection("Backend"));
+
+var backendApiConfig = new BackendApiConfig
+{
+    ConfigureBackendOptions = options => configuration.GetSection("WorkflowServer").Bind(options),
+    ConfigureHttpClientBuilder = options =>
+    {
+        options.ApiKey = configuration["Backend:ApiKey"];
+        options.BaseAddress = new("https://localhost:5001/elsa/api");
+        options.AuthenticationHandler = typeof(ApiKeyHttpMessageHandler); // TODO: replace with your own handler that e.g. attaches a bearer token.
+    }, 
+};
+
+var localizationConfig = new LocalizationConfig
+{
+    ConfigureLocalizationOptions = options => configuration.GetSection("Localization").Bind(options),
+};
 
 builder.Services.AddCore();
 builder.Services.AddShell();
-builder.Services.AddRemoteBackend();
+builder.Services.AddRemoteBackend(backendApiConfig);
 builder.Services.Replace(ServiceDescriptor.Scoped<IRemoteBackendAccessor, ComponentRemoteBackendAccessor>());
 builder.Services.AddWorkflowsModule();
+builder.Services.AddLocalizationModule(localizationConfig);
 
 var app = builder.Build();
 
