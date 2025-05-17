3c9a460270aee6ab2a11356ef548a219fe518e08
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-05-17 20:28:28 +0200

    Add custom Elsa Studio elements registration to Program.cs
    Registered custom Elsa Studio elements in the Blazor application's root components by invoking `RegisterCustomElsaStudioElements`. This enhances the application's integration with Elsa Studio workflows designer extensions.

--- src/blazor/ElsaStudioRehostedBlazorComponents/ElsaStudioRehostedBlazorComponents.Client/Program.cs
+++ src/blazor/ElsaStudioRehostedBlazorComponents/ElsaStudioRehostedBlazorComponents.Client/Program.cs
@@ -1,9 +1,11 @@
+using Elsa.Studio.Workflows.Designer.Extensions;
 using ElsaStudioRehostedBlazorComponents.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var configuration = builder.Configuration;
 var bootstrapper = new Bootstrapper(configuration, builder.Services);
+builder.RootComponents.RegisterCustomElsaStudioElements();
 bootstrapper.Configure();
 
 await builder.Build().RunAsync();
